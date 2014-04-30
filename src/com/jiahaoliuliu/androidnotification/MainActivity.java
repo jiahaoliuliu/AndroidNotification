caba2097155b9bf64d050c888cde37de0f723bb8
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-04-30 12:29:53 +0200

    Modified the intent to launch an app

--- src/com/jiahaoliuliu/androidnotification/MainActivity.java
+++ src/com/jiahaoliuliu/androidnotification/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 
@@ -14,7 +15,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Intent intent = new Intent(this, MainActivity.class);
+		PackageManager packageManager = getPackageManager();
+		Intent intent = packageManager.getLaunchIntentForPackage("com.jiahaoliuliu.guesssongs");
+		if (intent != null) {
+			// Set it to launch app
+			intent.addCategory(Intent.CATEGORY_LAUNCHER);
+		} else {
+			// If the app has not be found, do nothing
+			intent = new Intent();
+		}
 		PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
 		// build notification
