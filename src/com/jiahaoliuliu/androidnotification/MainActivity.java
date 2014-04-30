0c643a08fa3a319bf5bc46b02220cdff802f57c3
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-04-30 11:53:25 +0200

    Added the sample version to generate the notification

--- src/com/jiahaoliuliu/androidnotification/MainActivity.java
+++ src/com/jiahaoliuliu/androidnotification/MainActivity.java
@@ -1,7 +1,12 @@
 package com.jiahaoliuliu.androidnotification;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Intent intent = new Intent(this, MainActivity.class);
+		PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+		// build notification
+		// the addAction re-use the same intent to keep the example short
+		Notification n  = new NotificationCompat.Builder(this)
+		        .setContentTitle("New mail from " + "test@gmail.com")
+		        .setContentText("Subject")
+		        .setSmallIcon(R.drawable.ic_launcher)
+		        .setContentIntent(pIntent)
+		        .setAutoCancel(true).build();
+
+		NotificationManager notificationManager = 
+		  (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+		notificationManager.notify(0, n); 
 	}
 }
